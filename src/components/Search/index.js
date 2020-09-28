13d7c2148ede6bd74714b131d34144043a55f600
maxwelb-assis <max.assis@ymail.com>
2020-09-28 10:48:28 -0300

    finalizado o campo search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,35 @@
 import React from "react"
 
-const Search = () => <h1>Search</h1>
+import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
+
+import Hit from "./Hit"
+import * as S from "./styled"
+
+const algolia = {
+  appId: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+}
+
+
+const Search = () => (
+    <S.SearchWrapper>
+     <InstantSearch
+      appId={algolia.appId}
+      apiKey={algolia.searchOnlyApiKey}
+      indexName={algolia.indexName}
+    >
+      <SearchBox autoFocus translations={{ placeholder: "Pesquisar..." }} />
+      <Stats
+        translations={{
+          stats(nbHits, timeSpentMs) {
+            return `${nbHits} resultados encontrados em ${timeSpentMs}ms`
+          },
+        }}
+      />
+      <Hits hitComponent={Hit} />
+    </InstantSearch>
+  </S.SearchWrapper>
+)
 
 export default Search
